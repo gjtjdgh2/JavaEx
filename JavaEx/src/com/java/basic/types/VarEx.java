d0dea3b36f704cf3d0b5976a393266606ae80896
허성호 <gjtjdgh2@naver.com>
2021-02-25 16:26:34 +0900

    변수

---
+++ JavaEx/src/com/java/basic/types/VarEx.java
@@ -0,0 +1,22 @@
+package com.java.basic.types;
+
+public class VarEx {
+
+	public static void main(String[] args) {
+		//변수 식별자 규정
+		//문자 $ _ 숫자 조합
+		//숫자로 시박 하면 안됨
+		//예약어 사용 할수 없다
+		
+		// 관례:
+		//		소문자로 작성,단어의 조합일경우 뒷단어의 처음 글자는 대문자
+		int myAge;//빈 어있는 상태
+		myAge = 26;
+		System.out.println("내 나이는 = "+ myAge);
+		int v1 =10, v2=20,v3=30;
+		String b="a";
+		
+		
+	}
+
+}
